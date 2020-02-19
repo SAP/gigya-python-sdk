5fd4d7ab7f5e027c7e5228e888e30d3b0d019aff
alex.st <alex.strusberg@gigya-inc.com>
2020-02-19 09:43:14 +0200

    Story #107057 updating cacert.pm

--- GSSDK.py
+++ GSSDK.py
@@ -63,7 +63,7 @@ class GSException(Exception):
 
 class GSRequest():
     DEFAULT_API_DOMAIN = "us1.gigya.com"
-    VERSION = "3.3.3"
+    VERSION = "3.3.4"
     caCertsPath = os.path.join(os.path.dirname(__file__), "cacert.pem")
 
     _domain = ""
