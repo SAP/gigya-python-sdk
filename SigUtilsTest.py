534bf740c54d7f3e0a9d8d2bd02fd6b067d65dd1
kobi.cohen@sap.com <kobi.cohen@sap.com>
2021-05-20 09:58:24 +0300

    Add dependencies and build flow

--- SigUtilsTest.py
+++ SigUtilsTest.py
@@ -1,54 +1,57 @@
 import unittest
 import time
+
 try:
     from unittest.mock import patch
 except ImportError:
     from mock import patch
 from GSSDK import SigUtils
 
+
 class TestSigUtils(unittest.TestCase):
 
     @patch('GSSDK.SigUtils.calcSignature')
-    def test_validate_user_signature_use_expired_timestamp(self, mock_calcSignature):
-        mock_calcSignature.return_value = "signature"
-        UIDtimestamp = "1586977937"
-        
-        valid = SigUtils.validateUserSignature("UID", UIDtimestamp, "secret", "signature", 180)
+    def test_validate_user_signature_use_expired_timestamp(self, mock_calc_signature):
+        mock_calc_signature.return_value = "signature"
+        uid_timestamp = "1586977937"
+
+        valid = SigUtils.validateUserSignature("UID", uid_timestamp, "secret", "signature", 180)
         self.assertFalse(valid)
 
     @patch('GSSDK.SigUtils.calcSignature')
-    def test_validate_user_signature_use_valid_timestamp(self, mock_calcSignature):
-        mock_calcSignature.return_value = "signature"
+    def test_validate_user_signature_use_valid_timestamp(self, mock_calc_signature):
+        mock_calc_signature.return_value = "signature"
         now = int(round(time.time()) + 80)
-        UIDtimestamp = str(now)
-        
-        valid = SigUtils.validateUserSignature("UID", UIDtimestamp, "secret", "signature", 180)
+        uid_timestamp = str(now)
+
+        valid = SigUtils.validateUserSignature("UID", uid_timestamp, "secret", "signature", 180)
         self.assertTrue(valid)
 
     @patch('GSSDK.SigUtils.calcSignature')
-    def test_validate_user_signature_with_default_expiration_value(self, mock_calcSignature):
-        mock_calcSignature.return_value = "signature"
-        UIDtimestamp = "1586977937"
-        
-        valid = SigUtils.validateUserSignature("UID", UIDtimestamp, "secret", "signature")
+    def test_validate_user_signature_with_default_expiration_value(self, mock_calc_signature):
+        mock_calc_signature.return_value = "signature"
+        uid_timestamp = "1586977937"
+
+        valid = SigUtils.validateUserSignature("UID", uid_timestamp, "secret", "signature")
         self.assertTrue(valid)
 
     @patch('GSSDK.SigUtils.validateUserSignature')
-    def test_validate_user_signature_with_expiration_use_expired_timestamp(self, mock_validateUserSignature):
-        mock_validateUserSignature.return_value = True
-        UIDtimestamp = "1586977937"
-        
-        valid = SigUtils.validateUserSignatureWithExpiration("UID", UIDtimestamp, "secret", "signature", 180)    
+    def test_validate_user_signature_with_expiration_use_expired_timestamp(self, mock_validate_user_signature):
+        mock_validate_user_signature.return_value = True
+        uid_timestamp = "1586977937"
+
+        valid = SigUtils.validateUserSignatureWithExpiration("UID", uid_timestamp, "secret", "signature", 180)
         self.assertFalse(valid)
 
     @patch('GSSDK.SigUtils.validateUserSignature')
-    def test_validate_user_signature_with_expiration_use_valid_timestamp(self, mock_validateUserSignature):
-        mock_validateUserSignature.return_value = True
+    def test_validate_user_signature_with_expiration_use_valid_timestamp(self, mock_validate_user_signature):
+        mock_validate_user_signature.return_value = True
         now = int(round(time.time()) + 60)
-        UIDtimestamp = str(now)
+        uid_timestamp = str(now)
 
-        valid = SigUtils.validateUserSignatureWithExpiration("UID", UIDtimestamp, "secret", "signature", 180)
+        valid = SigUtils.validateUserSignatureWithExpiration("UID", uid_timestamp, "secret", "signature", 180)
         self.assertTrue(valid)
+
 
 if __name__ == '__main__':
     unittest.main()
